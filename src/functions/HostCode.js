e204f051a1d03c51cf47f1474c37888a1cbc99ee
Ben <benworrallpc@gmail.com>
2023-12-05 22:30:01 +0200

    access firebase db

--- src/functions/HostCode.js
+++ src/functions/HostCode.js
@@ -1,13 +1,25 @@
 import React from "react";
 import { txtDB } from "../firebase/firebaseConfig";
-import { ref } from "firebase/storage";
-import { addDoc, collection } from "firebase/firestore";
+import { collection, Firestore, getDocs, getFirestore, Query, QueryDocumentSnapshot, QuerySnapshot } from 'firebase/firestore'
 
+const db = getFirestore()
 
 
 const getRandomCode = async () => {
-
+    const ARcodes = []
+    const querySnapshot = await getDocs(collection(db, "AvailableCodes"));
+     querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      ARcodes.push(doc.data());
+    });
+    const finalCode = await ARcodes[1]
+    console.log(finalCode)
     
+  
+
+
+
+
 
 }
 export default getRandomCode
